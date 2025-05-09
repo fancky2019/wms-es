edc91e133a273a931b51f91bb526b540fbf83448
fancky2019 <709737588@qq.com>
2025-05-09 17:24:59 +0800

    命名

--- src/main/java/gs/com/gses/WmsEsApplication.java
+++ src/main/java/gs/com/gses/WmsEsApplication.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy(exposeProxy = true)//开启spring注解aop配置的支持，获取当前代理对象 (PersonService) AopContext.currentProxy();
 @SpringBootApplication
-public class GsesApplication {
+public class WmsEsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GsesApplication.class, args);
+        SpringApplication.run(WmsEsApplication.class, args);
     }
 
 }
