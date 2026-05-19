3825f12f350aefb84084ad9af55dd8cb1a7f22de
fancky2019 <709737588@qq.com>
2026-05-19 09:57:26 +0800

    事务传播开启新连接挂起旧连接事务

--- src/main/java/com/gs/gses/service/erp/impl/ErpProjectInfoViewImpl.java
+++ src/main/java/com/gs/gses/service/erp/impl/ErpProjectInfoViewImpl.java
@@ -31,9 +31,18 @@ public class ErpProjectInfoViewImpl extends ServiceImpl<ErpProjectInfoViewMapper
 
 
 //    事务内动态切换数据源，默认是切不过去的：Spring事务中的 Connection 是线程绑定的，当前事务已经绑定好的 MASTER Connection不会重新切换。
+
+
+   /*
+    REQUIRED	         融入或创建事务	    使用同一个连接
+    REQUIRES_NEW	     挂起并创建新事务	    先挂起旧连接，再创建使用新连接（新连接会关闭自动提交）
+    NOT_SUPPORTED	     挂起并以非事务运行	先挂起旧连接，再获取使用新连接（新连接保持自动提交）
+    */
+
+
     @DataSource(DataSourceType.THIRD)
 //    @Transactional(propagation = Propagation.REQUIRES_NEW, readOnly = true)
-    @Transactional(propagation = Propagation.NOT_SUPPORTED)  // 挂起当前事务
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)  // 挂起当前事务:开启新连接，挂起前一个连接
     @Override
     public PageData<ErpProjectInfoViewResponse> getErpProjectInfoViewPage(ErpProjectInfoViewRequest request) throws Exception {
         LambdaQueryWrapper<ErpProjectInfoView> projectInfoViewQueryWrapper = new LambdaQueryWrapper<>();
