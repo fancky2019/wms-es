db81d5091f4a12ab8209b749fefd75f435c07976
fancky2019 <709737588@qq.com>
2026-05-28 10:11:12 +0800

    多数据源获取dataSource原理

--- src/main/java/com/gs/gses/multipledatasource/DynamicDataSource.java
+++ src/main/java/com/gs/gses/multipledatasource/DynamicDataSource.java
@@ -15,8 +15,30 @@ public class DynamicDataSource extends AbstractRoutingDataSource {
     public DynamicDataSource(DataSource defaultTargetDataSource,
                              Map<Object, Object> targetDataSources) {
         super.setDefaultTargetDataSource(defaultTargetDataSource);
+        //AbstractRoutingDataSource类targetDataSources 会添加到resolvedDataSources
+        //resolvedDataSources根据determineCurrentLookupKey 的LookupKey 获取对应的dataSource
         super.setTargetDataSources(targetDataSources);
         super.afterPropertiesSet();
+
+
+        /*
+          AbstractRoutingDataSource 类根据 determineCurrentLookupKey() 获取dataSource逻辑。
+          根据LookupKey 取不到就用默认的defaultTargetDataSource
+            protected DataSource determineTargetDataSource() {
+        Assert.notNull(this.resolvedDataSources, "DataSource router not initialized");
+        Object lookupKey = this.determineCurrentLookupKey();
+        DataSource dataSource = (DataSource)this.resolvedDataSources.get(lookupKey);
+        if (dataSource == null && (this.lenientFallback || lookupKey == null)) {
+            dataSource = this.resolvedDefaultDataSource;
+        }
+
+        if (dataSource == null) {
+            throw new IllegalStateException("Cannot determine target DataSource for lookup key [" + lookupKey + "]");
+        } else {
+            return dataSource;
+        }
+    }
+         */
     }
 
     @Override
